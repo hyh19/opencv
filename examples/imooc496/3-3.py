b84ebe2918e6152e633ea5c56e3b2d58c9db130e
e7e26e5 <abc@xyz.com>
2023-02-21 17:29:12 +0800

    3-5

--- examples/imooc496/3-3.py
+++ examples/imooc496/3-3.py
@@ -1,7 +1,7 @@
 import cv2
 
 cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-img = cv2.imread('/Users/hyh/Downloads/IMG_0385.JPG')
+img = cv2.imread('/Users/hyh/Downloads/input.jpg')
 cv2.imshow('img', img)
 key = cv2.waitKey(0)
 # TODO 3-4.py 修复
--- examples/imooc496/3-4.py
+++ examples/imooc496/3-4.py
@@ -1,8 +1,8 @@
 import cv2
 
 cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-img = cv2.imread('/Users/hyh/Downloads/IMG_0385.JPG')
+img = cv2.imread('/Users/hyh/Downloads/input.jpg')
 cv2.imshow('img', img)
 key = cv2.waitKey(0)
-if key == ord('q'):
+if key & 0xFF == ord('q'):
     cv2.destroyAllWindows()
