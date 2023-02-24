8124469350ea129dc444e55b768fd10420069920
e7e26e5 <abc@xyz.com>
2023-02-24 18:33:48 +0800

    8-7

---
+++ examples/imooc496/8-7.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('lena.png')
+
+# 双边滤波
+dst = cv2.bilateralFilter(img, 7, 20, 50)
+
+cv2.imshow('dst', dst)
+cv2.imshow('img', img)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# bilateralFilter: t.ly/F0FP
