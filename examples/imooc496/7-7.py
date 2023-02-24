f9b9afacfa81bce6b5b42419dbc542430b8b3e96
e7e26e5 <abc@xyz.com>
2023-02-24 11:25:57 +0800

    7-7

---
+++ examples/imooc496/7-7.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('perspective.jpeg')
+
+src = np.float32([[100, 1100], [2100, 1100], [0, 4000], [2500, 3900]])
+dst = np.float32([[0, 0], [2300, 0], [0, 3000], [2300, 3000]])
+M = cv2.getPerspectiveTransform(src, dst)
+
+new = cv2.warpPerspective(img, M, (2300, 3000))
+
+cv2.imshow('img', img)
+cv2.imshow('new', new)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# getPerspectiveTransform: t.ly/LOKc
+# warpPerspective: t.ly/uj5Y
