419c81680e271ad059be86535e5344134f76b21b
e7e26e5 <abc@xyz.com>
2023-02-24 12:05:37 +0800

    8-3

---
+++ examples/imooc496/8-3.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('dog.jpeg')
+
+kernel = np.ones((5, 5), np.float32) / 25
+dst = cv2.filter2D(img, -1, kernel)
+
+cv2.imshow('dst', dst)
+cv2.imshow('img', img)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# filter2D: t.ly/COoA
