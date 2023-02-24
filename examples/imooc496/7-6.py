6f5b950f923fbde4bd18fe424cebcfe1ecd0668c
e7e26e5 <abc@xyz.com>
2023-02-24 11:12:04 +0800

    7-6

--- examples/imooc496/7-6.py
+++ examples/imooc496/7-6.py
@@ -1,12 +1,14 @@
 import cv2
+import numpy as np
 
 dog = cv2.imread('./dog.jpeg')
 h, w, ch = dog.shape
 
 print(dog.shape)
 
-# 旋转方向是逆时针，中心点是 (x, y)
-M = cv2.getRotationMatrix2D((w / 2, h / 2), 15, 1.0)
+src = np.float32([[400, 300], [800, 300], [400, 1000]])
+dst = np.float32([[200, 400], [600, 500], [150, 1100]])
+M = cv2.getAffineTransform(src, dst)
 
 # 如果想改变新图像的尺寸，需要修改 dsize
 new = cv2.warpAffine(dog, M, (w, h))
@@ -18,4 +20,4 @@ key = cv2.waitKey(0)
 if key & 0xFF == ord('q'):
     cv2.destroyAllWindows()
 
-# getRotationMatrix2D: t.ly/1g6M
+# getAffineTransform: t.ly/deh5
