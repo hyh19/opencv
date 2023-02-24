00d578348335f86000de4ca9901097247ef1aabb
e7e26e5 <abc@xyz.com>
2023-02-24 12:13:26 +0800

    8-4

---
+++ examples/imooc496/8-4.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread('dog.jpeg')
+
+# 均值滤波
+dst = cv2.blur(img, (5, 5))
+
+cv2.imshow('dst', dst)
+cv2.imshow('img', img)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# blur: t.ly/xzjZ
