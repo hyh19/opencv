839cb9471315ef073a618b32ea8b90a767f0ca8f
e7e26e5 <abc@xyz.com>
2023-02-25 22:15:56 +0800

    8-11

---
+++ examples/imooc496/8-11.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread('lena.png')
+
+dst = cv2.Canny(img, 100, 200)
+
+cv2.imshow('img', img)
+cv2.imshow('dst', dst)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# Canny: t.ly/AAXS
