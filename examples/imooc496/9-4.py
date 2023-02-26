86cbccbf5c20605f2e4f2703103e46368113c1fd
e7e26e5 <abc@xyz.com>
2023-02-26 20:22:56 +0800

    9-4

---
+++ examples/imooc496/9-4.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread('math.png')
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+dst = cv2.adaptiveThreshold(img1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                            cv2.THRESH_BINARY, 11, 0)
+print(dst.shape)
+
+cv2.imshow('img', img)
+cv2.imshow('gray', img1)
+cv2.imshow('bin', dst)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# adaptiveThreshold: t.ly/j29S
