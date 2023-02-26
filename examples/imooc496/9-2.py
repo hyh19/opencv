fe24856613b85ec31cfd42e1bc875e8cf1338cc1
e7e26e5 <abc@xyz.com>
2023-02-26 20:06:29 +0800

    9-2

---
+++ examples/imooc496/9-2.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread('dog.jpeg')
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, dst = cv2.threshold(img1, 180, 255, cv2.THRESH_BINARY)
+print(dst.shape)
+
+cv2.imshow('img', img)
+cv2.imshow('gray', img1)
+cv2.imshow('bin', dst)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# threshold: t.ly/BVBu
