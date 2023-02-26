3f7a431b1f1e12f5c14aefdf6eb6c83a754136ca
e7e26e5 <abc@xyz.com>
2023-02-26 21:05:26 +0800

    9-10

---
+++ examples/imooc496/9-10.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread('j.png')
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+print(kernel)
+
+dst = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+
+cv2.imshow('img', img)
+cv2.imshow('dst', dst)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# morphologyEx: t.ly/6hb9x
