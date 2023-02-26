d6795d05864632dd53efbe84e07b3fcc604c12c4
e7e26e5 <abc@xyz.com>
2023-02-26 20:50:12 +0800

    9-7

---
+++ examples/imooc496/9-7.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread('j.png')
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+print(kernel)
+
+dst = cv2.dilate(img, kernel, iterations=1)
+
+cv2.imshow('img', img)
+cv2.imshow('dst', dst)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# dilate: t.ly/Mkxq
