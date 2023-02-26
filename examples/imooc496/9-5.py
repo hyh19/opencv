85b7d08d1d7ce58709c60d8a38f875242649f7b3
e7e26e5 <abc@xyz.com>
2023-02-26 20:36:44 +0800

    9-5

---
+++ examples/imooc496/9-5.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('j.png')
+
+kernel = np.ones((3, 3), np.uint8)
+dst = cv2.erode(img, kernel, iterations=1)
+
+cv2.imshow('img', img)
+cv2.imshow('dst', dst)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# erode: t.ly/3GTc
