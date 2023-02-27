e2af7cd5806ff8d041f9096aa233f5e385cf5038
e7e26e5 <abc@xyz.com>
2023-02-27 10:27:54 +0800

    10-3

---
+++ examples/imooc496/10-3.py
@@ -0,0 +1,24 @@
+import cv2
+
+# 读文件
+img = cv2.imread('contours1.jpeg')
+
+# 转变成单通道
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 二值化
+ret, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+# 轮廓查找
+contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# 绘制轮廓
+cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
+
+cv2.imshow('img', img)
+
+key = cv2.waitKey(0)
+if key & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
+
+# findContours: t.ly/9M2x
