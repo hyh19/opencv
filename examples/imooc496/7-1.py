ca418c516c38c2ac0b5fcc73bbd273e5c4f87171
e7e26e5 <abc@xyz.com>
2023-03-05 11:41:41 +0800

    resize

--- examples/imooc496/7-1.py
+++ examples/imooc496/7-1.py
@@ -1,11 +1,12 @@
 import cv2
 
-dog = cv2.imread('./dog.jpeg')
-print(dog.shape)
+dog = cv2.imread('dog.jpeg')
 new = cv2.resize(dog, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
+
 cv2.imshow('dog', dog)
 cv2.imshow('new', new)
 
-key = cv2.waitKey(0)
-if key & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# resize: https://bit.ly/3SSovPW
