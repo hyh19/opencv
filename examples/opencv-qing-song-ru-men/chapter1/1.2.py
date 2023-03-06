541843b696665d8088c1bd8748950a908370c911
e7e26e5 <abc@xyz.com>
2023-03-06 16:09:28 +0800

    1.2

---
+++ examples/opencv-qing-song-ru-men/chapter1/1.2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+lena = cv2.imread("lena.bmp")
+cv2.namedWindow("lesson")
+cv2.imshow("lesson", lena)
+cv2.waitKey(0)
---
+++ examples/opencv-qing-song-ru-men/chapter1/1.2_1.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+lena = cv2.imread("lena.bmp")
+cv2.imshow("lesson", lena)
+cv2.waitKey(0)
