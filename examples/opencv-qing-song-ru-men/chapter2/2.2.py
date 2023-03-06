fe1543addd8dd9333aa15dca5b19e62bbca1b779
e7e26e5 <abc@xyz.com>
2023-03-06 17:06:56 +0800

    update

---
+++ examples/opencv-qing-song-ru-men/chapter2/2.2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+img = cv2.imread("lena.bmp", 0)
+cv2.imshow("before", img)
+for i in range(10, 100):
+    for j in range(80, 100):
+        img[i, j] = 255
+cv2.imshow("after", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
