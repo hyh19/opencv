66caef2242020423db7172b56e5e75f326153f38
e7e26e5 <abc@xyz.com>
2023-03-07 18:14:05 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter3/3.15.py
+++ examples/opencv-qing-song-ru-men/chapter3/3.15.py
@@ -2,16 +2,18 @@
 import cv2
 import numpy as np
 
+# 窗口截图 https://bit.ly/3kPPX4b
+
 # 读取原始载体图像
 lena = cv2.imread("lena.bmp", cv2.IMREAD_GRAYSCALE)
 # 读取水印图像
 watermark = cv2.imread("watermark.bmp", cv2.IMREAD_GRAYSCALE)
 # 将水印内的255处理为1，以方便嵌入
-# 后续章节会介绍使用threshold处理。
 w = watermark[:, :] > 0
 watermark[w] = 1
 # 读取原始载体图像的shape值
 r, c = lena.shape
+
 # ============嵌入过程============
 # 生成内部值都是254的数组
 t254 = np.ones((r, c), dtype=np.uint8) * 254
@@ -19,6 +21,7 @@ t254 = np.ones((r, c), dtype=np.uint8) * 254
 lenaH7 = cv2.bitwise_and(lena, t254)
 # 将watermark嵌入到lenaH7内
 e = cv2.bitwise_or(lenaH7, watermark)
+
 # ============提取过程============
 # 生成内部值都是1的数组
 t1 = np.ones((r, c), dtype=np.uint8)
@@ -26,7 +29,6 @@ t1 = np.ones((r, c), dtype=np.uint8)
 wm = cv2.bitwise_and(e, t1)
 print(wm)
 # 将水印内的1处理为255以方便显示
-# 后续章节会介绍threshold实现。
 w = wm[:, :] > 0
 wm[w] = 255
 
