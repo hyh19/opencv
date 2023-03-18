1799a7b20159db8a58260c68fab39c060ede8fd9
e7e26e5 <abc@xyz.com>
2023-03-18 22:40:23 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter5/5.12.py
+++ examples/opencv-qing-song-ru-men/chapter5/5.12.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  1 23:12:33 2018
-
-@author: 李立宗  lilizong@gmail.com
-《OpenCV图穷匕见——Python实现》 电子工业出版社
-"""
-
 import cv2
 import numpy as np
-img=np.random.randint(0,256,size=[4,5],dtype=np.uint8)
-rows,cols=img.shape
-mapx = np.zeros(img.shape,np.float32)
-mapy = np.zeros(img.shape,np.float32)
-for i in range(rows):
-    for j in range(cols):
-        mapx.itemset((i,j),j)
-        mapy.itemset((i,j),rows-1-i)
-rst=cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
-print("img=\n",img)
-print("mapx=\n",mapx)
-print("mapy=\n",mapy)
-print("rst=\n",rst)
+
+img = np.random.randint(0, 256, size=[4, 5], dtype=np.uint8)
+rows, cols = img.shape
+map_row = np.zeros(img.shape, np.float32)
+map_col = np.zeros(img.shape, np.float32)
+for r in range(rows):
+    for c in range(cols):
+        map_row.itemset((r, c), rows - 1 - r)
+        map_col.itemset((r, c), c)
+rst = cv2.remap(img, map_col, map_row, cv2.INTER_LINEAR)
+print("img=\n", img)
+print("map_row=\n", map_row)
+print("map_col=\n", map_col)
+print("rst=\n", rst)
--- examples/opencv-qing-song-ru-men/chapter5/5.20.py
+++ examples/opencv-qing-song-ru-men/chapter5/5.20.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  2 10:31:22 2018
-
-@author: 李立宗  lilizong@gmail.com
-《OpenCV图穷匕见——Python实现》 电子工业出版社
-"""
 import cv2
 import numpy as np
-img=cv2.imread("lena.bmp")
-rows,cols=img.shape[:2]
-mapx = np.zeros(img.shape[:2],np.float32)
-mapy = np.zeros(img.shape[:2],np.float32)
-for i in range(rows):
-    for j in range(cols):
-        if 0.25*cols< i <0.75*cols and 0.25*rows< j <0.75*rows:
-                mapx.itemset((i,j),2*( j - cols*0.25 ) + 0.5)
-                mapy.itemset((i,j),2*( i - rows*0.25 ) + 0.5)
-        else:     
-                mapx.itemset((i,j),0)
-                mapy.itemset((i,j),0)
-rst=cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
-cv2.imshow("original",img)
-cv2.imshow("result",rst)
+
+img = cv2.imread("lena.bmp")
+rows, cols = img.shape[:2]
+map_row = np.zeros(img.shape[:2], np.float32)
+map_col = np.zeros(img.shape[:2], np.float32)
+for r in range(rows):
+    for c in range(cols):
+        if 0.25 * rows < r < 0.75 * rows and 0.25 * cols < c < 0.75 * cols:
+            # 先缩放，后平移
+            map_row.itemset((r, c), 2 * r - rows * 0.5 + 0.5)
+            map_col.itemset((r, c), 2 * c - cols * 0.5 + 0.5)
+        else:
+            map_row.itemset((r, c), 0)
+            map_col.itemset((r, c), 0)
+rst = cv2.remap(img, map_col, map_row, cv2.INTER_LINEAR)
+cv2.imshow("original", img)
+cv2.imshow("result", rst)
 cv2.waitKey()
 cv2.destroyAllWindows()
