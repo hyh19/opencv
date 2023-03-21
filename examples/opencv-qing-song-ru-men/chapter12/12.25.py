c4403bbff6decfd43a43ce45151a4878fa2c5cea
e7e26e5 <abc@xyz.com>
2023-03-21 18:52:59 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter12/12.25.py
+++ examples/opencv-qing-song-ru-men/chapter12/12.25.py
@@ -1,40 +1,46 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 14 18:38:25 2018
-
-@author: 李立宗  lilizong@gmail.com
-"""
-
 import cv2
-#----------------原始图像-------------------------
+
+# ----------------原始图像-------------------------
 o = cv2.imread('cs.bmp')
-cv2.imshow("original",o)
-#----------------获取凸包------------------------ 
-gray = cv2.cvtColor(o,cv2.COLOR_BGR2GRAY)  
-ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)  
-image,contours, hierarchy = cv2.findContours(binary,
-                                             cv2.RETR_LIST,
-                                             cv2.CHAIN_APPROX_SIMPLE)  
+cv2.imshow("original", o)
+
+# ----------------获取凸包------------------------
+gray = cv2.cvtColor(o, cv2.COLOR_BGR2GRAY)
+ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+image, contours, hierarchy = cv2.findContours(binary,
+                                              cv2.RETR_LIST,
+                                              cv2.CHAIN_APPROX_SIMPLE)
 hull = cv2.convexHull(contours[0])
 image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 cv2.polylines(image, [hull], True, (0, 255, 0), 2)
-#----------------内部点A与多边形的关系-------------------------
-distA = cv2.pointPolygonTest(hull, (300, 150),False)  
-font=cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(image,'A',(300,150), font, 1,(0,255,0),3)
-print("distA=",distA) 
-#----------------外部点B与多边形的关系-------------------------
-distB = cv2.pointPolygonTest(hull, (300, 250), False)  
-font=cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(image,'B',(300,250), font, 1,(0,255,0),3)
-print("distB=",distB) 
-#----------------边缘线上点C与多边形的关系----------------------
-distC = cv2.pointPolygonTest(hull, (423, 112),False)  
-font=cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(image,'C',(423,112), font, 1,(0,255,0),3)
-print("distC=",distC) 
-#print(hull)   #测试边缘到底在哪里，然后再使用确定位置的
-#----------------显示-------------------------
-cv2.imshow("result",image)
+
+# ----------------内部点A的距离-------------------------
+ptA = (300, 150)
+distA = cv2.pointPolygonTest(hull, ptA, False)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(image, 'A', ptA, font, 1, (0, 255, 0), 3)
+cv2.circle(image, ptA, 3, [255, 0, 0], -1)
+print("distA=", distA)
+
+# ----------------外部点B的距离-------------------------
+ptB = (300, 250)
+distB = cv2.pointPolygonTest(hull, ptB, False)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(image, 'B', ptB, font, 1, (0, 255, 0), 3)
+cv2.circle(image, ptB, 3, [255, 0, 0], -1)
+print("distB=", distB)
+
+# ------------正好处于边缘上的点C的距离-----------------
+ptC = (423, 112)
+distC = cv2.pointPolygonTest(hull, ptC, False)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(image, 'C', ptC, font, 1, (0, 255, 0), 3)
+cv2.circle(image, ptC, 3, [255, 0, 0], -1)
+print("distC=", distC)
+# print(hull)   #测试边缘到底在哪里，然后再使用确定位置的
+
+# ----------------显示-------------------------
+cv2.imshow("result", image)
 cv2.waitKey()
 cv2.destroyAllWindows()
