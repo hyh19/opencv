f92a6da46dbf6be71c3659fb5983e6bce026c45c
e7e26e5 <abc@xyz.com>
2023-03-22 18:16:51 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter2/2.1.py
+++ examples/opencv-qing-song-ru-men/chapter2/2.1.py
@@ -4,11 +4,13 @@ import numpy as np
 
 img = np.zeros((8, 8), dtype=np.uint8)
 print("img=\n", img)
-cv2.imshow("one", img)
 print("读取像素点img[0,3]=", img[0, 3])
+cv2.imshow("one", img)
+
 img[0, 3] = 255
 print("修改后img=\n", img)
 print("读取修改后像素点img[0,3]=", img[0, 3])
 cv2.imshow("two", img)
+
 cv2.waitKey()
 cv2.destroyAllWindows()
