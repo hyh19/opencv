45df4f1a599196ea9e759ef61a21d8e216256806
e7e26e5 <abc@xyz.com>
2023-03-24 18:32:43 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter08/8.05.py
+++ examples/opencv-qing-song-ru-men/chapter08/8.05.py
@@ -2,10 +2,10 @@
 import cv2 as cv
 import numpy as np
 
-o = cv.imread('dilation.bmp', cv.IMREAD_UNCHANGED)
+img = cv.imread('dilation.bmp', cv.IMREAD_UNCHANGED)
 kernel = np.ones((9, 9), np.uint8)
-dilation = cv.dilate(o, kernel)
-cv.imshow('original', o)
+dilation = cv.dilate(img, kernel)
+cv.imshow('img', img)
 cv.imshow('dilation', dilation)
 cv.waitKey()
 cv.destroyAllWindows()
