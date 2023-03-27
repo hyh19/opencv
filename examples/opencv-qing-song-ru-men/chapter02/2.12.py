40813e183b22f5ea743bef4bf5da400bdd5b70e2
e7e26e5 <abc@xyz.com>
2023-03-27 10:06:13 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter02/2.12.py
+++ examples/opencv-qing-song-ru-men/chapter02/2.12.py
@@ -10,10 +10,10 @@ print('读取img.item(0,0,0)=', img.item(0, 0, 0))
 print('读取img.item(0,0,1)=', img.item(0, 0, 1))
 print('读取img.item(0,0,2)=', img.item(0, 0, 2))
 
-for i in range(0, 50):
-    for j in range(0, 100):
-        for k in range(0, 3):
-            img.itemset((i, j, k), 255)  # 白色
+for row in range(0, 50):
+    for col in range(0, 100):
+        for ch in range(0, 3):
+            img.itemset((row, col, ch), 255)  # 白色
 
 cv.imshow('after', img)
 print('修改后img.item(0,0,0)=', img.item(0, 0, 0))
