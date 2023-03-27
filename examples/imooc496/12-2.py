1ab26c996b38e893e3f51895c3e6a04a07100ae4
e7e26e5 <abc@xyz.com>
2023-03-27 19:47:32 +0800

    update

--- examples/imooc496/12-2.py
+++ examples/imooc496/12-2.py
@@ -1,8 +1,7 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
 
-# 运行结果 https://is.gd/nfLHXA
+# 运行结果 https://is.gd/JKuhVr
 
 img = cv.imread('water_coins.jpeg')
 cv.imshow("img", img)
@@ -19,28 +18,6 @@ cv.imshow("open", open1)
 
 dilate = cv.dilate(open1, kernel, iterations=1)
 cv.imshow("dilate", dilate)
-
-dist = cv.distanceTransform(open1, cv.DIST_L2, 5)
-# If the image is 32-bit or 64-bit floating-point, the pixel values are multiplied by 255.
-# That is, the value range [0,1] is mapped to [0,255].
-# cv.imshow("dist", dist)
-plt.imshow(dist, cmap='gray')
-plt.show()
-
-_, fg = cv.threshold(dist, 0.7 * dist.max(), 255, cv.THRESH_BINARY)
-cv.imshow("fg", fg)
-
-fg = np.uint8(fg)
-unknown = cv.subtract(dilate, fg)
-cv.imshow("unknown", unknown)
-
-ret, markers = cv.connectedComponents(fg)
-markers = markers + 1
-markers[unknown == 255] = 0
-
-markers = cv.watershed(img, markers)
-img[markers == -1] = [0, 0, 255]
-cv.imshow("result", img)
 
 cv.waitKey()
 cv.destroyAllWindows()
