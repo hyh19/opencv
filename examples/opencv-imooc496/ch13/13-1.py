ff8d61f70b88ff23ce8324443663f0422013bfc9
e7e26e5 <abc@xyz.com>
2023-04-02 17:19:53 +0800

    update

--- examples/opencv-imooc496/ch13/13-1.py
+++ examples/opencv-imooc496/ch13/13-1.py
@@ -2,10 +2,10 @@ import cv2 as cv
 
 # 运行结果 https://is.gd/wK6glU
 
-face_classifier = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+classifier = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 img = cv.imread('p3.png')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces = face_classifier.detectMultiScale(gray, 1.1, 3)
+faces = classifier.detectMultiScale(gray, 1.1, 3)
 for (x, y, w, h) in faces:
     cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 cv.imshow('img', img)
--- examples/opencv-imooc496/ch13/13-3.py
+++ examples/opencv-imooc496/ch13/13-3.py
@@ -4,12 +4,12 @@ import pytesseract
 img = cv.imread('chinacar.jpeg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-car_plate_classifier = cv.CascadeClassifier('haarcascades/haarcascade_russian_plate_number.xml')
-car_plates = car_plate_classifier.detectMultiScale(gray, 1.1, 3)
-for (x, y, w, h) in car_plates:
+classifier = cv.CascadeClassifier('haarcascades/haarcascade_russian_plate_number.xml')
+plates = classifier.detectMultiScale(gray, 1.1, 3)
+for (x, y, w, h) in plates:
     cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
     gray_roi = gray[y:y + h, x:x + w]
-    _, gray_roi_bin = cv.threshold(gray_roi, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    _, gray_roi_bin = cv.threshold(gray_roi, 0, 255, cv.THRESH_OTSU)
     text = pytesseract.image_to_string(gray_roi_bin, lang='chi_sim+eng', config='--psm 8 --oem 3')
     print(text)
 
