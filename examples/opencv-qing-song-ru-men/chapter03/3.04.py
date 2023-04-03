d3b02672727a3800013ba8bdf06608a1f3947ea5
e7e26e5 <abc@xyz.com>
2023-04-03 18:49:17 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter03/3.04.py
+++ examples/opencv-qing-song-ru-men/chapter03/3.04.py
@@ -6,4 +6,21 @@ img1 = np.ones((3, 4), dtype=np.uint8) * 100
 img2 = np.ones((3, 4), dtype=np.uint8) * 10
 gamma = 3
 img3 = cv.addWeighted(img1, 0.6, img2, 5, gamma)
-print(img3)
+print('img1=\n', img1)
+print('img2=\n', img2)
+print('img3=\n', img3)
+
+'''
+img1=
+ [[100 100 100 100]
+ [100 100 100 100]
+ [100 100 100 100]]
+img2=
+ [[10 10 10 10]
+ [10 10 10 10]
+ [10 10 10 10]]
+img3=
+ [[113 113 113 113]
+ [113 113 113 113]
+ [113 113 113 113]]
+'''
