073ad39ed22b3bef11a50796af02f5423f423858
e7e26e5 <abc@xyz.com>
2023-04-03 10:16:03 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter03/3.05.py
+++ examples/opencv-qing-song-ru-men/chapter03/3.05.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 
-a = cv.imread("boat.bmp")
-b = cv.imread("lena.bmp")
-result = cv.addWeighted(a, 0.6, b, 0.4, 0)
-cv.imshow("boat", a)
-cv.imshow("lena", b)
+# 运行结果 https://is.gd/CRt5AO
+
+boat = cv.imread("boat.bmp")
+lena = cv.imread("lena.bmp")
+result = cv.addWeighted(boat, 0.6, lena, 0.4, 0)
+cv.imshow("boat", boat)
+cv.imshow("lena", lena)
 cv.imshow("result", result)
 cv.waitKey()
 cv.destroyAllWindows()
--- examples/opencv-qing-song-ru-men/chapter03/3.06.py
+++ examples/opencv-qing-song-ru-men/chapter03/3.06.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 
+# 运行结果 https://is.gd/ycuVnN
+
 lena = cv.imread("lena512.bmp", cv.IMREAD_UNCHANGED)
 dollar = cv.imread("dollar.bmp", cv.IMREAD_UNCHANGED)
 cv.imshow("lena", lena)
 cv.imshow("dollar", dollar)
-face1 = lena[220:400, 250:350]
-face2 = dollar[160:340, 200:300]
-add = cv.addWeighted(face1, 0.6, face2, 0.4, 0)
-dollar[160:340, 200:300] = add
+
+# Extract face regions
+lena_face = lena[220:400, 250:350]
+dollar_face = dollar[160:340, 200:300]
+
+# Blend the faces
+blended_face = cv.addWeighted(lena_face, 0.6, dollar_face, 0.4, 0)
+
+# Replace the dollar face with the blended face
+dollar[160:340, 200:300] = blended_face
+
+# Show the result
 cv.imshow("result", dollar)
 cv.waitKey()
 cv.destroyAllWindows()
