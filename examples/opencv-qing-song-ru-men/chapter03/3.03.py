82bd4f159a20a5e35b982cbf7abb6a974ce4483b
e7e26e5 <abc@xyz.com>
2023-04-03 18:43:54 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter03/3.03.py
+++ examples/opencv-qing-song-ru-men/chapter03/3.03.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 
-img_gray = cv.imread('lena.bmp', cv.IMREAD_GRAYSCALE)
-img_copy = img_gray.copy()
-result1 = img_gray + img_copy
-result2 = cv.add(img_gray, img_copy)
-cv.imshow('Original', img_gray)
-cv.imshow('Result1', result1)
-cv.imshow('Result2', result2)
+# 运行结果 https://is.gd/3kOFmH
+
+gray = cv.imread('lena.bmp', cv.IMREAD_GRAYSCALE)
+result1 = gray + gray
+result2 = cv.add(gray, gray)
+cv.imshow('gray', gray)
+cv.imshow('result1', result1)
+cv.imshow('result2', result2)
 cv.waitKey()
 cv.destroyAllWindows()
