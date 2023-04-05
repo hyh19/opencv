3a99211bd0a76226009a20dfe85c052405840c4f
e7e26e5 <abc@xyz.com>
2023-04-05 11:12:44 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter12/12.01.py
+++ examples/opencv-qing-song-ru-men/chapter12/12.01.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 
-o = cv.imread('contours.bmp')
-cv.imshow("original", o)
-gray = cv.cvtColor(o, cv.COLOR_BGR2GRAY)
-ret, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
-image, contours, hierarchy = cv.findContours(binary,
-                                              cv.RETR_EXTERNAL,
-                                              cv.CHAIN_APPROX_SIMPLE)
-o = cv.drawContours(o, contours, -1, (0, 0, 255), 5)
-cv.imshow("result", o)
+# 运行结果 https://is.gd/wVD8zw
+
+img = cv.imread('contours.bmp')
+cv.imshow("img", img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+_, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+_, contours, hierarchy = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(img, contours, -1, (0, 0, 255), 5)
+cv.imshow("result", img)
 cv.waitKey()
 cv.destroyAllWindows()
+
+'''
+>>> print(hierarchy)
+[[[ 1 -1 -1 -1]
+  [ 2  0 -1 -1]
+  [-1  1 -1 -1]]]
+
+>>> type(contours)
+<class 'tuple'>
+
+>>> type(contours[0])
+<class 'numpy.ndarray'>
+
+>>> print(contours[0].shape)
+(4, 1, 2)
+'''
