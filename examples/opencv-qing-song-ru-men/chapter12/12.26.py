4375cd90fabef72cbadfc532bbeb8225d7f6ff59
e7e26e5 <abc@xyz.com>
2023-04-06 12:17:13 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter12/12.26.py
+++ examples/opencv-qing-song-ru-men/chapter12/12.26.py
@@ -1,47 +1,27 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 
-# -----------原始图像o1边缘--------------------
-o1 = cv.imread('cs.bmp')
-cv.imshow("original1", o1)
-gray1 = cv.cvtColor(o1, cv.COLOR_BGR2GRAY)
-ret, binary1 = cv.threshold(gray1, 127, 255, cv.THRESH_BINARY)
-image, contours1, hierarchy = cv.findContours(binary1,
-                                               cv.RETR_LIST,
-                                               cv.CHAIN_APPROX_SIMPLE)
-cnt1 = contours1[0]
+# 运行结果 https://is.gd/AiyDQM
 
-# -----------原始图像o2边缘--------------------
-o2 = cv.imread('cs3.bmp')
-cv.imshow("original2", o2)
-gray2 = cv.cvtColor(o2, cv.COLOR_BGR2GRAY)
-ret, binary2 = cv.threshold(gray2, 127, 255, cv.THRESH_BINARY)
-image, contours2, hierarchy = cv.findContours(binary2,
-                                               cv.RETR_LIST,
-                                               cv.CHAIN_APPROX_SIMPLE)
-cnt2 = contours2[0]
+image_names = ['cs.bmp', 'cs3.bmp', 'hand.bmp']
+contours_list = []
 
-# -----------原始图像o3边缘--------------------
-o3 = cv.imread('hand.bmp')
-cv.imshow("original3", o3)
-gray3 = cv.cvtColor(o3, cv.COLOR_BGR2GRAY)
-ret, binary3 = cv.threshold(gray3, 127, 255, cv.THRESH_BINARY)
-image, contours3, hierarchy = cv.findContours(binary3,
-                                               cv.RETR_LIST,
-                                               cv.CHAIN_APPROX_SIMPLE)
-cnt3 = contours3[0]
+for i, file_name in enumerate(image_names):
+    img = cv.imread(file_name)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, binary = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+    _, contours, _ = cv.findContours(binary, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(img, contours, 0, (0, 0, 255), 2)
+    cv.imshow(f'img{i}', img)
+    contours_list.append(contours[0])
 
-# -----------构造距离提取算子--------------------
-sd = cv.createShapeContextDistanceExtractor()
+shape_extractor = cv.createShapeContextDistanceExtractor()
+d1 = shape_extractor.computeDistance(contours_list[0], contours_list[0])
+print(f'与自身的距离 d1 = {d1}')
+d2 = shape_extractor.computeDistance(contours_list[0], contours_list[1])
+print(f'与旋转缩放后对象的距离 d2 = {d2}')
+d3 = shape_extractor.computeDistance(contours_list[0], contours_list[2])
+print(f'与不相似对象的距离 d3 = {d3}')
 
-# -----------计算距离--------------------
-d1 = sd.computeDistance(cnt1, cnt1)
-print("自身距离d1=", d1)
-d2 = sd.computeDistance(cnt1, cnt2)
-print("旋转缩放后距离d2=", d2)
-d3 = sd.computeDistance(cnt1, cnt3)
-print("不相似对象距离d3=", d3)
-
-# -----------显示距离--------------------
 cv.waitKey()
 cv.destroyAllWindows()
