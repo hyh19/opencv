17bbc6efef745e28917bd71073eb4701ff20a111
e7e26e5 <abc@xyz.com>
2023-04-09 21:50:53 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter10/10.1.py
+++ examples/opencv-qing-song-ru-men/chapter10/10.1.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
 
-o = cv.imread("lena.bmp", cv.IMREAD_GRAYSCALE)
-r1 = cv.Canny(o, 128, 200)
-r2 = cv.Canny(o, 32, 128)
-cv.imshow("original", o)
-cv.imshow("result1", r1)
-cv.imshow("result2", r2)
+gray_img = cv.imread("lena.bmp", cv.IMREAD_GRAYSCALE)
+canny_img1 = cv.Canny(gray_img, 128, 200)
+canny_img2 = cv.Canny(gray_img, 32, 128)
+cv.imshow("gray", gray_img)
+cv.imshow("canny1", canny_img1)
+cv.imshow("canny2", canny_img2)
 cv.waitKey()
 cv.destroyAllWindows()
