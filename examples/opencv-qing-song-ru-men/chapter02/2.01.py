27ffc2c52ec536b019d0153579f917bca166a1a7
e7e26e5 <abc@xyz.com>
2023-05-04 23:59:41 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter02/2.01.py
+++ examples/opencv-qing-song-ru-men/chapter02/2.01.py
@@ -3,14 +3,32 @@ import cv2 as cv
 import numpy as np
 
 img = np.zeros((8, 8), dtype=np.uint8)
-print('img=\n', img)
-print('读取像素点img[0,3]=', img[0, 3])
-cv.imshow('one', img)
+print(f'img = \n{img}')
+print(f'读取像素点 img[0,3] = {img[0, 3]}')
 
 img[0, 3] = 255
-print('修改后img=\n', img)
-print('读取修改后像素点img[0,3]=', img[0, 3])
-cv.imshow('two', img)
+print(f'修改后 img = \n{img}')
+print(f'读取修改后像素点 img[0,3] = {img[0, 3]}')
 
-cv.waitKey()
-cv.destroyAllWindows()
+'''
+img = 
+[[0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]
+ [0 0 0 0 0 0 0 0]]
+读取像素点 img[0,3] = 0
+修改后 img = 
+[[  0   0   0 255   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]
+ [  0   0   0   0   0   0   0   0]]
+读取修改后像素点 img[0,3] = 255
+'''
--- examples/opencv-qing-song-ru-men/chapter02/2.02.py
+++ examples/opencv-qing-song-ru-men/chapter02/2.02.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 
-img = cv.imread('lena.bmp', cv.IMREAD_GRAYSCALE)
-cv.imshow('before', img)
+img_gray = cv.imread('lena.bmp', cv.IMREAD_GRAYSCALE)
+cv.imshow('before', img_gray)
 
 for row in range(10, 100):
     for col in range(80, 100):
-        img[row, col] = 255
-cv.imshow('after', img)
+        img_gray[row, col] = 255
+cv.imshow('after', img_gray)
 
 cv.waitKey()
 cv.destroyAllWindows()
+
+# 运行结果 https://is.gd/7J3nnq
