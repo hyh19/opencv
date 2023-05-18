38ea89f364c621c4640babb0950bc468ae93d09f
e7e26e5 <abc@xyz.com>
2023-05-18 19:08:44 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter04/4.13.py
+++ examples/opencv-qing-song-ru-men/chapter04/4.13.py
@@ -5,9 +5,7 @@ bgr_img = cv.imread('lenacolor.png')
 cv.imshow('bgr', bgr_img)
 
 bgra_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2BGRA)
-blue_img, green_img, red_img, alpha_img = cv.split(bgra_img)
-alpha_list = [255, 125, 0]
-for alpha in alpha_list:
+for alpha in [255, 125, 0]:
     bgra_img[:, :, 3] = alpha
     cv.imshow(f'bgr_alpha_{alpha}', bgra_img)
     cv.imwrite(f'bgr_alpha_{alpha}.png', bgra_img)
