863e8120b41014dda6b79b7688aa21cd2aedfd84
e7e26e5 <abc@xyz.com>
2023-06-03 11:41:40 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter17/17.1.py
+++ examples/opencv-qing-song-ru-men/chapter17/17.1.py
@@ -3,11 +3,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("rice.png", cv2.IMREAD_UNCHANGED)
+img = cv2.imread("rice.png")
 kernel = np.ones((5, 5), np.uint8)
-erode = cv2.erode(img, kernel)
-subtract = cv2.subtract(img, erode)
+img_erode = cv2.erode(img, kernel)
+img_subtract = cv2.subtract(img, img_erode)
 plt.subplot(131), plt.imshow(img), plt.axis('off')
-plt.subplot(132), plt.imshow(erode), plt.axis('off')
-plt.subplot(133), plt.imshow(subtract), plt.axis('off')
+plt.subplot(132), plt.imshow(img_erode), plt.axis('off')
+plt.subplot(133), plt.imshow(img_subtract), plt.axis('off')
 plt.show()
+
+# 运行结果 https://is.gd/kEXNv7
--- examples/opencv-qing-song-ru-men/chapter17/17.2.py
+++ examples/opencv-qing-song-ru-men/chapter17/17.2.py
@@ -4,18 +4,17 @@ import cv2
 import matplotlib.pyplot as plt
 
 img = cv2.imread('water_coins.jpg')
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-_, binary_inv = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, img_bin_inv = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 kernel = np.ones((3, 3), np.uint8)
-open1 = cv2.morphologyEx(binary_inv, cv2.MORPH_OPEN, kernel, iterations=2)
+img_open = cv2.morphologyEx(img_bin_inv, cv2.MORPH_OPEN, kernel, iterations=2)
 
-dist = cv2.distanceTransform(open1, cv2.DIST_L2, 5)
-_, fg = cv2.threshold(dist, 0.7 * dist.max(), 255, cv2.THRESH_BINARY)
+img_dist = cv2.distanceTransform(img_open, cv2.DIST_L2, 5)
+_, img_fg = cv2.threshold(img_dist, 0.7 * img_dist.max(), 255, cv2.THRESH_BINARY)
 
 plt.subplot(131), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)), plt.axis('off')
-plt.subplot(132), plt.imshow(dist, cmap='gray'), plt.axis('off')
-plt.subplot(133), plt.imshow(fg, cmap='gray'), plt.axis('off')
+plt.subplot(132), plt.imshow(img_dist, cmap='gray'), plt.axis('off')
+plt.subplot(133), plt.imshow(img_fg, cmap='gray'), plt.axis('off')
 plt.show()
+
+# 运行结果 https://is.gd/e5bl4J
