9f270e77ae03f1650de83e17cbc45aa543959e59
e7e26e5 <abc@xyz.com>
2023-06-04 12:31:54 +0800

    update

--- examples/opencv-qing-song-ru-men/chapter17/17.7.py
+++ examples/opencv-qing-song-ru-men/chapter17/17.7.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import cv2
+import cv2 as cv
 import matplotlib.pyplot as plt
 
-# 代码解释 https://is.gd/SrUJ6f
-# 运行结果 https://is.gd/pz5wM8
-
-img = cv2.imread('lenacolor.png')
+img = cv.imread('lenacolor.png')
 
 mask = np.zeros(img.shape[:2], np.uint8)
-bgdModel = np.zeros((1, 65), np.float64)
-fgdModel = np.zeros((1, 65), np.float64)
+bgd_model = np.zeros((1, 65), np.float64)
+fgd_model = np.zeros((1, 65), np.float64)
 rect = (50, 50, 400, 400)
 
-cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+cv.grabCut(img, mask, rect, bgd_model, fgd_model, 5, cv.GC_INIT_WITH_RECT)
 mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 result = img * mask[:, :, np.newaxis]
 
-cv2.rectangle(img, (50, 50), (450, 450), (255, 0, 0), 3)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv.rectangle(img, (50, 50), (450, 450), (255, 0, 0), 3)
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 plt.subplot(121), plt.imshow(img), plt.axis('off')
 
-result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+result = cv.cvtColor(result, cv.COLOR_BGR2RGB)
 plt.subplot(122), plt.imshow(result), plt.axis('off')
 
 plt.show()
+
+# 运行结果 https://is.gd/pz5wM8
--- examples/opencv-qing-song-ru-men/chapter17/17.8.py
+++ examples/opencv-qing-song-ru-men/chapter17/17.8.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import cv2
+import cv2 as cv
 import matplotlib.pyplot as plt
 
-# 运行结果 https://is.gd/hc1pTn
-
-img = cv2.imread('lenacolor.png')
-img_mask = cv2.imread('mask.png')
-img_mask_gray = cv2.cvtColor(img_mask, cv2.COLOR_BGR2GRAY)
+img = cv.imread('lenacolor.png')
+img_mask = cv.imread('mask.png')
+img_mask_gray = cv.cvtColor(img_mask, cv.COLOR_BGR2GRAY)
 
 mask = np.zeros(img.shape[:2], np.uint8)
-bgd = np.zeros((1, 65), np.float64)
-fgd = np.zeros((1, 65), np.float64)
+bgd_model = np.zeros((1, 65), np.float64)
+fgd_model = np.zeros((1, 65), np.float64)
 rect = (50, 50, 400, 500)
 
 # 第一次处理 GC_INIT_WITH_RECT
-cv2.grabCut(img, mask, rect, bgd, fgd, 5, cv2.GC_INIT_WITH_RECT)
+cv.grabCut(img, mask, rect, bgd_model, fgd_model, 5, cv.GC_INIT_WITH_RECT)
 mask1 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 result1 = img * mask1[:, :, np.newaxis]
 
@@ -23,21 +21,23 @@ mask[img_mask_gray == 0] = 0
 mask[img_mask_gray == 255] = 1
 
 # 第二次处理 GC_INIT_WITH_MASK
-cv2.grabCut(img, mask, None, bgd, fgd, 5, cv2.GC_INIT_WITH_MASK)
+cv.grabCut(img, mask, None, bgd_model, fgd_model, 5, cv.GC_INIT_WITH_MASK)
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 result2 = img * mask2[:, :, np.newaxis]
 
-cv2.rectangle(img, (50, 50), (450, 450), (255, 0, 0), 3)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv.rectangle(img, (50, 50), (450, 450), (255, 0, 0), 3)
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 plt.subplot(221), plt.imshow(img), plt.axis('off'), plt.title('img')
 
-result1 = cv2.cvtColor(result1, cv2.COLOR_BGR2RGB)
+result1 = cv.cvtColor(result1, cv.COLOR_BGR2RGB)
 plt.subplot(222), plt.imshow(result1), plt.axis('off'), plt.title('result1')
 
-img_mask = cv2.cvtColor(img_mask, cv2.COLOR_BGR2RGB)
+img_mask = cv.cvtColor(img_mask, cv.COLOR_BGR2RGB)
 plt.subplot(223), plt.imshow(img_mask), plt.axis('off'), plt.title('img_mask')
 
-result2 = cv2.cvtColor(result2, cv2.COLOR_BGR2RGB)
+result2 = cv.cvtColor(result2, cv.COLOR_BGR2RGB)
 plt.subplot(224), plt.imshow(result2), plt.axis('off'), plt.title('result2')
 
 plt.show()
+
+# 运行结果 https://is.gd/hc1pTn
