6805a878697d640a3c2447ad502f53b0f36c6846
e7e26e5 <abc@xyz.com>
2023-06-04 11:53:26 +0800

    update

--- examples/opencv-imooc496/ch12/12-7.py
+++ examples/opencv-imooc496/ch12/12-7.py
@@ -1,71 +1,67 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 
 class App:
-    flag_rect = False
-    rect = (0, 0, 0, 0)
-    startX = 0
-    startY = 0
+    def __init__(self):
+        self.img = None
+        self.img_original = None
+        self.mask = None
+        self.output = None
+        self.flag_rect = False
+        self.rect = (0, 0, 0, 0)
+        self.startX = 0
+        self.startY = 0
 
-    def onmouse(self, event, x, y, flags, param):
-
-        if event == cv2.EVENT_LBUTTONDOWN:
+    def on_mouse(self, event, x, y, flags, param):
+        if event == cv.EVENT_LBUTTONDOWN:
             self.flag_rect = True
             self.startX = x
             self.startY = y
-            print("LBUTTIONDOWN")
-        elif event == cv2.EVENT_LBUTTONUP:
+        elif event == cv.EVENT_LBUTTONUP:
             self.flag_rect = False
-            cv2.rectangle(self.img,
-                          (self.startX, self.startY),
-                          (x, y),
-                          (0, 0, 255),
-                          3)
+            cv.rectangle(self.img,
+                         (self.startX, self.startY),
+                         (x, y),
+                         (0, 0, 255),
+                         3)
             self.rect = (min(self.startX, x), min(self.startY, y),
                          abs(self.startX - x),
                          abs(self.startY - y))
-
-            print("LBUTTIONUP")
-        elif event == cv2.EVENT_MOUSEMOVE:
-            if self.flag_rect == True:
-                self.img = self.img2.copy()
-                cv2.rectangle(self.img,
-                              (self.startX, self.startY),
-                              (x, y),
-                              (255, 0, 0),
-                              3)
-            print("MOUSEMOVE")
-
-        print("onmouse")
+        elif event == cv.EVENT_MOUSEMOVE:
+            if self.flag_rect:
+                self.img = self.img_original.copy()
+                cv.rectangle(self.img,
+                             (self.startX, self.startY),
+                             (x, y),
+                             (255, 0, 0),
+                             3)
 
     def run(self):
-        print("run...")
+        cv.namedWindow('input')
+        cv.setMouseCallback('input', self.on_mouse)
 
-        cv2.namedWindow('input')
-        cv2.setMouseCallback('input', self.onmouse)
-
-        self.img = cv2.imread('../lena.png')
-        self.img2 = self.img.copy()
+        self.img = cv.imread('lena.png')
+        self.img_original = self.img.copy()
         self.mask = np.zeros(self.img.shape[:2], dtype=np.uint8)
         self.output = np.zeros(self.img.shape, np.uint8)
 
-        while (1):
-            cv2.imshow('input', self.img)
-            cv2.imshow('output', self.output)
-            k = cv2.waitKey(100)
+        while True:
+            cv.imshow('input', self.img)
+            cv.imshow('output', self.output)
+            k = cv.waitKey(100)
             if k == 27:
                 break
 
             if k == ord('g'):
-                bgdmodel = np.zeros((1, 65), np.float64)
-                fgdmodel = np.zeros((1, 65), np.float64)
-                cv2.grabCut(self.img2, self.mask, self.rect,
-                            bgdmodel, fgdmodel,
-                            1,
-                            cv2.GC_INIT_WITH_RECT)
-            mask2 = np.where((self.mask == 1) | (self.mask == 3), 255, 0).astype('uint8')
-            self.output = cv2.bitwise_and(self.img2, self.img2, mask=mask2)
+                bgd_model = np.zeros((1, 65), np.float64)
+                fgd_model = np.zeros((1, 65), np.float64)
+                cv.grabCut(self.img_original, self.mask, self.rect,
+                           bgd_model, fgd_model,
+                           1,
+                           cv.GC_INIT_WITH_RECT)
+            tmp_mask = np.where((self.mask == 1) | (self.mask == 3), 255, 0).astype('uint8')
+            self.output = cv.bitwise_and(self.img_original, self.img_original, mask=tmp_mask)
 
 
 App().run()
