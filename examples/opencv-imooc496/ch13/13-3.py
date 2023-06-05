2eff20bfedf77dec9f493d9b6e97e4713e8173a8
e7e26e5 <abc@xyz.com>
2023-06-05 10:45:51 +0800

    update

--- examples/opencv-imooc496/ch13/13-3.py
+++ examples/opencv-imooc496/ch13/13-3.py
@@ -2,17 +2,19 @@ import cv2 as cv
 import pytesseract
 
 img = cv.imread('chinacar.jpeg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 classifier = cv.CascadeClassifier('haarcascades/haarcascade_russian_plate_number.xml')
-plates = classifier.detectMultiScale(gray, 1.1, 3)
-for (x, y, w, h) in plates:
+plates = classifier.detectMultiScale(img_gray, 1.1, 3)
+result = ''
+for x, y, w, h in plates:
     cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    gray_roi = gray[y:y + h, x:x + w]
-    _, gray_roi_bin = cv.threshold(gray_roi, 0, 255, cv.THRESH_OTSU)
-    text = pytesseract.image_to_string(gray_roi_bin, lang='chi_sim+eng', config='--psm 8 --oem 3')
-    print(text)
+    img_gray_roi = img_gray[y:y + h, x:x + w]
+    _, img_gray_roi_bin = cv.threshold(img_gray_roi, 0, 255, cv.THRESH_OTSU)
+    result = pytesseract.image_to_string(img_gray_roi_bin, lang='chi_sim+eng', config='--psm 8 --oem 3')
 
-cv.imshow('img', img)
+cv.imshow(f'Result: {result}', img)
 cv.waitKey()
 cv.destroyAllWindows()
+
+# 运行结果 https://is.gd/ZDvZ01
