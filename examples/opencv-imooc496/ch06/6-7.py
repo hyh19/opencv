e193c494a019901316c579f083f90ed16376fa4b
e7e26e5 <abc@xyz.com>
2023-08-18 18:33:02 +0800

    refactor

--- examples/opencv-imooc496/ch06/6-7.py
+++ examples/opencv-imooc496/ch06/6-7.py
@@ -1,22 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-# 创建 logo
-logo = np.zeros((200, 200, 3), np.uint8)
-logo[20:120, 20:120] = [0, 0, 255]
-logo[80:180, 80:180] = [0, 255, 0]
-cv.imshow('logo', logo)
-
-# 创建 mask
-mask = np.zeros((200, 200), np.uint8)
-mask[20:120, 20:120] = 255
-mask[80:180, 80:180] = 255
-cv.imshow('mask', mask)
-
-# 对 mask 按位求反
-mask_not = cv.bitwise_not(mask)
-cv.imshow('mask_not', mask_not)
-
 # 导入图片
 dog = cv.imread('../images/dog.jpeg')
 
@@ -24,14 +8,26 @@ dog = cv.imread('../images/dog.jpeg')
 roi = dog[0:200, 0:200]
 cv.imshow('roi', roi)
 
-# 抠出 logo 的位置
-tmp = cv.bitwise_and(roi, roi, mask=mask_not)
-cv.imshow('tmp', tmp)
+# 创建 logo
+logo = np.zeros((200, 200, 3), np.uint8)
+logo[20:120, 20:120] = [0, 0, 255]
+logo[80:180, 80:180] = [0, 255, 0]
+cv.imshow('logo', logo)
 
-dst = cv.add(tmp, logo)
-dog[0:200, 0:200] = dst
-cv.imshow('dst', dst)
+# 创建 mask
+mask = np.full((200, 200), 255, np.uint8)
+mask[20:120, 20:120] = 0
+mask[80:180, 80:180] = 0
+cv.imshow('mask', mask)
 
+# 抠出 logo 的位置（核心代码）
+roi_mask = cv.bitwise_and(roi, roi, mask=mask)
+cv.imshow('roi_mask', roi_mask)
+
+roi_logo = cv.add(roi_mask, logo)
+cv.imshow('roi_logo', roi_logo)
+
+dog[0:200, 0:200] = roi_logo
 cv.imshow('dog', dog)
 
 key = cv.waitKey(0)
