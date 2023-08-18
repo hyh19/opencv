0887b06bc12da188edac7962bc54c264cb39a434
e7e26e5 <abc@xyz.com>
2023-08-18 19:46:39 +0800

    refactor

--- examples/opencv-imooc496/ch09/9-2.py
+++ examples/opencv-imooc496/ch09/9-2.py
@@ -1,14 +1,13 @@
 import cv2 as cv
 
 img = cv.imread('../images/dog.jpeg')
-img1 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-ret, dst = cv.threshold(img1, 180, 255, cv.THRESH_BINARY)
-print(dst.shape)
+thresh, img_bin = cv.threshold(img_gray, thresh=180, maxval=255, type=cv.THRESH_BINARY)
 
 cv.imshow('img', img)
-cv.imshow('gray', img1)
-cv.imshow('bin', dst)
+cv.imshow('img_gray', img_gray)
+cv.imshow('img_bin', img_bin)
 
 key = cv.waitKey(0)
 if key & 0xFF == ord('q'):
