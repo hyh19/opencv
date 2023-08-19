78c504ddcffce889ae4bee7f0663bccaec421813
e7e26e5 <abc@xyz.com>
2023-08-19 12:27:04 +0800

    refactor

--- examples/opencv-imooc496/ch13/13-3.py
+++ examples/opencv-imooc496/ch13/13-3.py
@@ -5,15 +5,16 @@ img = cv.imread('chinacar.jpeg')
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 classifier = cv.CascadeClassifier('haarcascades/haarcascade_russian_plate_number.xml')
-plates = classifier.detectMultiScale(img_gray, 1.1, 3)
-result = ''
-for x, y, w, h in plates:
-    cv.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    img_gray_roi = img_gray[y:y + h, x:x + w]
-    _, img_gray_roi_bin = cv.threshold(img_gray_roi, 0, 255, cv.THRESH_OTSU)
-    result = pytesseract.image_to_string(img_gray_roi_bin, lang='chi_sim+eng', config='--psm 8 --oem 3')
 
-cv.imshow(f'Result: {result}', img)
+plates = classifier.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=3)
+
+for x, y, w, h in plates:
+    cv.rectangle(img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 255), thickness=2)
+    img_roi = img_gray[y:y + h, x:x + w]
+    _, img_bin = cv.threshold(img_roi, thresh=0, maxval=255, type=cv.THRESH_OTSU)
+    result = pytesseract.image_to_string(img_bin, lang='chi_sim+eng', config='--psm 8 --oem 3')
+    cv.imshow(f'Result: {result}', img)
+
 cv.waitKey()
 cv.destroyAllWindows()
 
--- examples/opencv-imooc496/ch13/13-5.py
+++ examples/opencv-imooc496/ch13/13-5.py
@@ -3,18 +3,18 @@ from cv2 import dnn
 import numpy as np
 
 # 配置文件、模型文件和分类标签文件路径
-config_path = 'model/bvlc_googlenet.prototxt'
-model_path = 'model/bvlc_googlenet.caffemodel'
+prototxt = 'model/bvlc_googlenet.prototxt'
+caffe_model = 'model/bvlc_googlenet.caffemodel'
 class_path = 'model/synset_words.txt'
 
 # 加载神经网络和分类标签
-network = dnn.readNetFromCaffe(config_path, model_path)
+network = dnn.readNetFromCaffe(prototxt, caffe_model)
 with open(class_path, 'rt') as f:
     class_names = [line[line.find(' ') + 1:].strip() for line in f]
 
 # 图像预处理和前向传递
 image = cv.imread('smallcat.jpeg')
-blob = dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
+blob = dnn.blobFromImage(image, scalefactor=1, size=(224, 224), mean=(104, 117, 123))
 network.setInput(blob)
 predictions = network.forward()
 
@@ -27,3 +27,11 @@ for i in range(top_n):
     class_index = indexes[i]
     class_name = class_names[class_index]
     print(f'Top {i + 1} [class: {class_name}], [line: {class_index + 1}], [score: {scores[i]}]')
+
+'''
+运行结果：
+
+Top 1 [class: tabby, tabby cat], [line: 282], [score: 0.2257317304611206]
+Top 2 [class: Egyptian cat], [line: 286], [score: 0.1566489040851593]
+Top 3 [class: Persian cat], [line: 284], [score: 0.1038256511092186]
+'''
